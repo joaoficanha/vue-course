5208353d358f181e93df983df9cd5360e998a063
João Pedro Ficanha <jpfficanha@gmail.com>
2023-03-13 14:37:24 -0300

    Section 21: project 05 monty hall
    Initialize Project

---
+++ projects/project-05-monty-hall/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Mounty Hall Problem</h1>
+    <Door />
+  </div>
+</template>
+
+<script>
+import Door from "./components/DoorComponent.vue";
+
+export default {
+  name: "App",
+  components: { Door },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
